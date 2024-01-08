704cf1b42bd77fd29aeb2fc2eaa725be2b4895e5
kasir <kasiramsureshm719@gmail.com>
2024-01-08 22:42:06 +0530

    First commit

---
+++ Band-Name-Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city=input("What's ur grown up city name?/n")
+suggestingname=input("What name could you suggest for ur music band?/n")
+print("Your band name could be " + city + suggestingname)
